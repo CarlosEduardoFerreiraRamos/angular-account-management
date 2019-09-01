c0c9c482c7ad5f030ec770c46139e2a94799f409
Carlos Ramos <carlosefrfloresta@gmail.com>
2019-09-01 17:00:05 -0300

    Table implemented

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, TemplateRef, EventEmitter, ContentChild } from '@angular/core';
+
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent implements OnInit {
+export class ListComponent {
 
-  constructor() { }
+  @Input() displayedColumns: string[];
 
-  ngOnInit() {
+  @Input()
+  get data(): any[] { return this._data; }
+  set data(v: any[]) {
+    if (v) {
+      this._data = v;
+
+      this.columns = this.data[0];
+    }
+  }
+
+  get columns(): any { return this._columns; }
+  set columns(v: any) {
+    if (v) {
+      if (this.displayedColumns) {
+        this._columns = this.displayedColumns;
+      } else {
+        this._columns = Object.keys(v);
+      }
+    }
+  }
+
+  @Output() rowClick = new EventEmitter();
+
+  @ContentChild(TemplateRef, {static: false}) content: TemplateRef<any>;
+
+  private _columns;
+
+  private _data: any[];
+
+  onRowClick(row) {
+    this.rowClick.emit(row);
+  }
+
+  get isInteractable(): boolean {
+    return !!this.rowClick.observers.length;
   }
 
 }
--- src/app/components/list/list.module.ts
+++ src/app/components/list/list.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListComponent } from './list.component';
+import { MatTableModule } from '@angular/material/table';
 
 
 
 @NgModule({
   declarations: [ListComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MatTableModule
+  ],
+  exports: [ListComponent]
 })
 export class ListModule { }
