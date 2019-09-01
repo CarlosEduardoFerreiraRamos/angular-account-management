7a75470bd7113861c0177254821751afa57d4a7a
Carlos Ramos <carlosefrfloresta@gmail.com>
2019-09-01 16:51:42 -0300

    Implemented styles in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouteConfigLoadStart, RouteConfigLoadEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-account-manager';
+export class AppComponent  implements OnInit {
+
+  isRoutLoading = false;
+
+  constructor (private router: Router) {}
+
+   ngOnInit () {
+    this.router.events.subscribe(event => {
+      if (event instanceof RouteConfigLoadStart) {
+          this.isRoutLoading = true;
+      } else if (event instanceof RouteConfigLoadEnd) {
+          this.isRoutLoading = false;
+      }
+    });
+  }
 }
