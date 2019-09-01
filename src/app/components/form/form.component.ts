fb411478efa5319e051abb5fd958935444b6019d
Carlos Ramos <carlosefrfloresta@gmail.com>
2019-09-01 17:06:55 -0300

    Implementation form component

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ContentChildren, QueryList, AfterViewInit, ViewChild, Output, EventEmitter } from '@angular/core';
+import { NgModel, NgForm } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
-export class FormComponent implements OnInit {
+export class FormComponent implements AfterViewInit {
 
-  constructor() { }
+  @ContentChildren(NgModel, {descendants: true}) models: QueryList<NgModel>;
 
-  ngOnInit() {
+  @ViewChild(NgForm, { static: false }) form: NgForm;
+
+  @Output() valueChanges = new EventEmitter();
+
+  @Output() formSubmit = new EventEmitter();
+
+  ngAfterViewInit() {
+    this.subscribeToValueChanges();
+    this.addContentModels();
+  }
+
+  private subscribeToValueChanges() {
+    this.form.valueChanges
+    .pipe(debounceTime(300))
+    .subscribe( values => {
+      this.valueChanges.emit(values);
+    });
+  }
+
+  private addContentModels() {
+    this.models.forEach((model) => {
+      this.form.addControl(model);
+    });
   }
 
 }
